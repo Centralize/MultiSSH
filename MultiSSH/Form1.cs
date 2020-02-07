a1e638b3cd6a2dfe1a613e1e16b7b6d01d38be09
Mikkel Kaas <59764907+mkaeucnv@users.noreply.github.com>
2020-02-07 12:42:29 +0100

    Initial commit

---
+++ MultiSSH/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+using Microsoft.Data.Sqlite;
+
+namespace MultiSSH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private ConnectionInfo SSHconnection(string host, string username, string password)
+        {
+            ConnectionInfo ConnNfo = new ConnectionInfo(host, 22, username,
+                new AuthenticationMethod[]{
+                    new PasswordAuthenticationMethod(username,password),
+                }
+            );
+
+            return ConnNfo;
+        }
+
+        private void SSHexecution(ConnectionInfo ConnNfo, string Command)
+        {
+            using (var sshClient = new SshClient(ConnNfo))
+            {
+                sshClient.Connect();
+                sshClient.CreateCommand(Command);
+                sshClient.Disconnect();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "MultiSSH";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConnectionInfo ConnInfo = SSHconnection("dock2.thy-it.com", "root", "Ert67yu89io");
+            SSHexecution(ConnInfo, "whoami");
+        }
+    }
+}
